008e956143db94c30c0bd4259dd578647b56db71
prithvirajbarad <112259618+prithvirajbarad@users.noreply.github.com>
2023-10-27 10:22:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+let formEle = document.querySelector("form");
+
+formEle.addEventListener("submit", submitForm);
+
+function submitForm(e){
+	e.preventDefault();
+	let checkEle = document.getElementById("checkbox").checked;
+	alert( `${checkEle}`);
+}
