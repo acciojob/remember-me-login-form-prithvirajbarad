e7743d563e8fa09557c611db57212a5b508947ea
prithvirajbarad <112259618+prithvirajbarad@users.noreply.github.com>
2023-11-02 09:51:35 +0530

    created a submission

--- script.js
+++
@@ -1,6 +0,0 @@
-function getFormvalue() {
-    //Write your code here
-	let first = document.getElementById("first").value;
-	let last = document.getElementById("last").value;
-	alert(first+" "+last);
-}
